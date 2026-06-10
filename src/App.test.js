6f36acd60877224b6c4805af361a6cc606a168fb
Giancarlo <g_biasiucci@outlook.com>
2026-06-10 11:54:47 -0400

    Update failing default-generated React app test with correct link text

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders parody link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/What this website is intended to be/i);
   expect(linkElement).toBeInTheDocument();
 });
